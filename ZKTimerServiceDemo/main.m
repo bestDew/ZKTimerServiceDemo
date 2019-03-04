0af2b1ddeffe9343531cba2dd1bbf44d705b1085
bestdew <a_pple0429@163.com>
2019-03-04 17:24:33 +0800

    Initial Commit

---
+++ ZKTimerServiceDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZKTimerServiceDemo
+//
+//  Created by bestdew on 2019/3/4.
+//  Copyright © 2019 bestdew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
